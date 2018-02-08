a81119cc217f858e77906ef45da609c01bcc20e5
teilon <teiloncw@gmail.com>
2018-02-08 19:45:18 +0600

    add temp products to site

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path('index/$', views.index, name='index'),
     re_path('column/$', views.column, name='column'),
     re_path('group/$', views.group, name='group'),
+    re_path('^product/(?P<name>.+)/$', views.product, name='product'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,121 @@ from django.shortcuts import render, redirect
 
 
 def home(request):
-    context = {}
+
+    list = [
+        {
+            'name': '«Орман» СО-Э711',
+            'text': 'Статический счетчик.',
+            'price': '5 900 ₸',
+            'price_min': '5 850 ₸',
+            'img': 'CC.jpg'
+        },
+        {
+            'name': '«Орман» СО-Э711',
+            'text': 'Статический счетчик.',
+            'price': '2 900 ₸',
+            'price_min': '2 850 ₸',
+            'img': 'CE.jpg'
+        },
+        {
+            'name': '«Орман» СО-Э711',
+            'text': 'Статический счетчик.',
+            'price': '18 200 ₸',
+            'price_min': '18 100 ₸',
+            'img': 'CL.jpg'
+        },
+        {
+            'name': '«Орман» СО-Э711 PLC',
+            'text': 'Статический счетчик.',
+            'price': '22 000 ₸',
+            'price_min': '21 900 ₸',
+            'img': 'CU.jpg'
+        },
+        {
+            'name': '«Орман» Т1 СО-Э711',
+            'text': 'Статический счетчик.',
+            'price': '2 900 ₸',
+            'price_min': '2 850 ₸',
+            'img': 'CV.jpg'
+        },
+        {
+            'name': '«Дала» СА4-Э720',
+            'text': 'Счетчик прямого включения.',
+            'price': '10 400 ₸',
+            'price_min': '10 350 ₸',
+            'img': 'NA.jpg'
+        },
+        {
+            'name': '«Дала» СА4-Э720 Т1',
+            'text': 'Счетчик прямого включения.',
+            'price': '11 600 ₸',
+            'price_min': '11 550 ₸',
+            'img': 'NB.jpg'
+        },
+        {
+            'name': '«Дала» СА4-Э720 ТХ',
+            'text': 'Счетчик прямого включения.',
+            'price': '13 600 ₸',
+            'price_min': '13 550 ₸',
+            'img': 'NC.jpg'
+        },
+        {
+            'name': '«Дала» СА4У-Э720',
+            'text': 'Счетчик трансформаторного включения.',
+            'price': '9 400 ₸',
+            'price_min': '9 350 ₸',
+            'img': 'ND.jpg'
+        },
+        {
+            'name': '«Дала» СА4У-Э720 Т1',
+            'text': 'Счетчик трансформаторного включения.',
+            'price': '11 200 ₸',
+            'price_min': '11 150 ₸',
+            'img': 'NE.jpg'
+        },
+        {
+            'name': '«Дала» СА4У-Э720 ТХ',
+            'text': 'Счетчик трансформаторного включения.',
+            'price': '12 900 ₸',
+            'price_min': '12 850 ₸',
+            'img': 'NF.jpg'
+        },
+        {
+            'name': '«Дала» TX P PLC IP П RS',
+            'text': 'Электронный счетчик.',
+            'price': '42 000 ₸',
+            'price_min': '41 800 ₸',
+            'img': 'NL.jpg'
+        },
+        {
+            'name': '«Дала» СА3У-Э720',
+            'text': 'Счетчик трансформаторного включения.',
+            'price': '9 700 ₸',
+            'price_min': '9 650 ₸',
+            'img': 'NM.jpg'
+        },
+        {
+            'name': '«Дала» СА4-Э720',
+            'text': 'Счетчик прямого включения.',
+            'price': '14 000 ₸',
+            'price_min': '13 950 ₸',
+            'img': 'NX.jpg'
+        },
+        {
+            'name': '«Дала» СА4-Э720 ТХ',
+            'text': 'Счетчик прямого включения.',
+            'price': '18 400 ₸',
+            'price_min': '18 350 ₸',
+            'img': 'NY.jpg'
+        },
+    ]
+    title_list = ['«Орман»',
+                  '«Дала»',
+                  ]
+
+    context = {'list': list,
+               'title_list': title_list,
+               }
     return render(request, 'app/home.html', context)
 
 
@@ -19,3 +133,8 @@ def column(request):
 def group(request):
     context = {}
     return render(request, 'app/group.html', context)
+
+
+def product(request, name):
+    context = {'name': name}
+    return render(request, 'app/product.html', context)
